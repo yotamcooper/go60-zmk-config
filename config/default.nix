7ff01c36bebc5baf12fbd1c1801e9ac57d786806
yotamcooper <yotam.cooper@gmail.com>
2026-03-14 23:03:42 +0200

    Update default.nix

--- config/default.nix
+++ config/default.nix
@@ -5,7 +5,7 @@
 let
   config = ./.;
 
-  go60_left  = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; overlay = "${config}/go60_lh.overlay"; };
+  go60_left  = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
   go60_right = firmware.zmk.override { board = "go60_rh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
 
 in firmware.combine_uf2 go60_left go60_right "go60"
